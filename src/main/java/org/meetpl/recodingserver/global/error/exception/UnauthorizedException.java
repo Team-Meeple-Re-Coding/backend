aff55db065d0a19e00830cc86853e5786d67e417
RyuKwanKon <rhks33rhs@gmail.com>
2024-04-01 09:57:15 +0900

    [feat] #1 UnauthorizedException 구현

---
+++ src/main/java/org/meetpl/recodingserver/global/error/exception/UnauthorizedException.java
@@ -0,0 +1,13 @@
+package org.meetpl.recodingserver.global.error.exception;
+
+import org.meetpl.recodingserver.global.error.ErrorCode;
+
+public class UnauthorizedException extends BusinessException {
+    public UnauthorizedException() {
+        super(ErrorCode.UNAUTHORIZED);
+    }
+
+    public UnauthorizedException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+}

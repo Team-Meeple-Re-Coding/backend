a50eb045dadf6b6ac085d0bc18698c93afd87f1e
RyuKwanKon <rhks33rhs@gmail.com>
2024-04-01 09:15:55 +0900

    [feat] #1 init

---
+++ src/main/java/org/meetpl/recodingserver/ReCodingServerApplication.java
@@ -0,0 +1,13 @@
+package org.meetpl.recodingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReCodingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReCodingServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/meetpl/recodingserver/ReCodingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.meetpl.recodingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReCodingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

4c11cc011bddf664da4e1ac14b2fa25f58512779
RyuKwanKon <rhks33rhs@gmail.com>
2024-04-01 09:53:39 +0900

    [feat] #1 JpaConfig 구현

---
+++ src/main/java/org/meetpl/recodingserver/global/config/JpaConfig.java
@@ -0,0 +1,11 @@
+package org.meetpl.recodingserver.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
+
+

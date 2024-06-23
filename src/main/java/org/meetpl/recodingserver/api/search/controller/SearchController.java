97cbe68c8ba8c9609b50a60c9234812e197fc33c
Bae Hyeonseo <haulqogustj@naver.com>
2024-06-23 10:44:57 +0900

    [feat]: #5 컨트롤러 수정

--- src/main/java/org/meetpl/recodingserver/api/search/controller/SearchController.java
+++ src/main/java/org/meetpl/recodingserver/api/search/controller/SearchController.java
@@ -5,11 +5,9 @@ import org.meetpl.recodingserver.api.search.service.SearchService;
 import org.meetpl.recodingserver.api.search.service.dto.req.SearchReqDto;
 import org.meetpl.recodingserver.api.search.service.dto.res.SearchResDto;
 import org.meetpl.recodingserver.global.common.SuccessResponse;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,8 +17,8 @@ import java.util.List;
 public class SearchController {
     private SearchService searchService;
     @PostMapping
-    public ResponseEntity<SuccessResponse<?>> searchReviewer(@RequestBody SearchReqDto searchReqDto){
-        SearchResDto searchResDto = searchService.searchReviewers(searchReqDto);
+    public ResponseEntity<SuccessResponse<?>> searchReviewer(@RequestParam(value = "page", defaultValue = "0") int page, @RequestBody SearchReqDto searchReqDto){
+        SearchResDto searchResDto = searchService.searchReviewers(PageRequest.of(page, 9) ,searchReqDto);
         return SuccessResponse.ok(searchResDto);
     }
 }

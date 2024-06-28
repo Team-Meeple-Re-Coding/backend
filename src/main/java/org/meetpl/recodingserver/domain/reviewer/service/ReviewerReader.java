61f1cdd7125f391b8bab76840ee6042d84af66cc
RyuKwanKon <rhks33rhs@gmail.com>
2024-06-28 16:52:37 +0900

    [Feat] #9 reviewer 상세 정보 조회 메서드 구현

--- src/main/java/org/meetpl/recodingserver/domain/reviewer/service/ReviewerReader.java
+++ src/main/java/org/meetpl/recodingserver/domain/reviewer/service/ReviewerReader.java
@@ -1,19 +1,28 @@
 package org.meetpl.recodingserver.domain.reviewer.service;
 
 import lombok.RequiredArgsConstructor;
-import org.meetpl.recodingserver.api.search.service.dto.req.SearchReqDto;
+import org.meetpl.recodingserver.api.search.dto.req.SearchReqDto;
 import org.meetpl.recodingserver.domain.reviewer.domain.Reviewer;
-import org.meetpl.recodingserver.domain.reviewer.repository.ReviewerRepository;
+import org.meetpl.recodingserver.domain.reviewer.dto.ReviewerDetailDto;
 import org.meetpl.recodingserver.domain.reviewer.repository.ReviewerRepositoryImpl;
+import org.meetpl.recodingserver.global.error.exception.EntityNotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import static org.meetpl.recodingserver.global.error.ErrorCode.REVIEWER_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
 public class ReviewerReader {
     private final ReviewerRepositoryImpl reviewerRepository;
+
     public Page<Reviewer> getReviewerBySearchReqDto(Pageable pageable, SearchReqDto searchReqDto) {
         return reviewerRepository.findReviewerBySearchReqDto(pageable, searchReqDto);
+    }
+
+    public ReviewerDetailDto findReviewerDetailById(Long reviewId) {
+        return reviewerRepository.findReviewerDetailById(reviewId)
+                .orElseThrow(() -> new EntityNotFoundException(REVIEWER_NOT_FOUND));
     }
 }

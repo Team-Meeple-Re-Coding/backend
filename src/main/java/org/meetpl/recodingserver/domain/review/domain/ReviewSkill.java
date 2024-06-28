58d5c379e48fe0c0ca46a392960ab66572a64f51
RyuKwanKon <rhks33rhs@gmail.com>
2024-06-28 16:50:12 +0900

    [Feat] #9 code review & reviewer 관계 재정의

--- src/main/java/org/meetpl/recodingserver/domain/review/domain/ReviewSkill.java
+++ src/main/java/org/meetpl/recodingserver/domain/review/domain/ReviewSkill.java
@@ -1,20 +1,22 @@
-package org.meetpl.recodingserver.domain.reviewer.domain;
+package org.meetpl.recodingserver.domain.review.domain;
+
 import jakarta.persistence.*;
 import lombok.*;
+import org.meetpl.recodingserver.domain.reviewer.domain.SkillType;
 
-@Entity
-@Table(name = "skill")
-@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder(access = AccessLevel.PRIVATE)
-public class Skill {
+@Table(name = "reviewe_skill")
+@Getter
+@Entity
+public class ReviewSkill {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Enumerated(EnumType.STRING)
     private SkillType skillType;
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "reviewer_id")
-    private Reviewer reviewer;
+    @JoinColumn(name = "review_id")
+    private Review review;
 }

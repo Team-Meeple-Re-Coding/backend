9f813cd45aceece2fa8602865b975b341fa9c1d2
RyuKwanKon <rhks33rhs@gmail.com>
2024-06-28 16:47:39 +0900

    [Fix] #9 code review mapping 관계 재정의

--- src/main/java/org/meetpl/recodingserver/domain/codereview/domain/CodeReview.java
+++ src/main/java/org/meetpl/recodingserver/domain/codereview/domain/CodeReview.java
@@ -1,7 +1,9 @@
 package org.meetpl.recodingserver.domain.codereview.domain;
 import jakarta.persistence.*;
 import lombok.*;
+import org.meetpl.recodingserver.domain.reviewee.domain.Reviewee;
 import org.meetpl.recodingserver.domain.reviewer.domain.Reviewer;
+import org.meetpl.recodingserver.global.common.BaseTimeEntity;
 
 @Entity
 @Table(name = "code_review")
@@ -9,12 +11,16 @@ import org.meetpl.recodingserver.domain.reviewer.domain.Reviewer;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder(access = AccessLevel.PRIVATE)
-public class CodeReview {
+public class CodeReview extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String request;
     private String projectInfo;
-    @OneToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reviewer_id")
     private Reviewer reviewer;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reviewee_id")
+    private Reviewee reviewee;
 }
